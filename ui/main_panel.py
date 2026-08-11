70ad33c7f7d128d05b85df02480d5f2375960f16
arcsikex <arcsikex@hotmail.com>
2026-08-11 15:32:10 +0200

    Refactor main UI

--- ui/main_panel.py
+++ ui/main_panel.py
@@ -21,85 +21,124 @@ class CBB_PT_parent_panel(PanelBasics, bpy.types.Panel):
         _ch = CascadeurHandler()
         addon_props = context.scene.cbb_fbx_settings
         layout = self.layout
-        col = layout.column(align=False)
 
-        errors = []
-
-        if not _ch.is_csc_exe_path_valid:
-            errors.append("No valid Cascadeur exe path set in preferences!")
-
-        if not _ch.is_csc_bridge_installed:
-            errors.append("Cascadeur side of the add-on is not installed!")
-
-        if errors:
-            for error in errors:
-                col.label(icon="ERROR", text=error)
-
-            col.separator()
-            col.operator(
-                "cbb.open_preferences",
-                text="Open Preferences",
-                icon="PREFERENCES",
-            )
+        if not self._draw_verification(layout, _ch):
             return
 
-        row = col.row()
+        if self._draw_operation_status(layout):
+            return
 
+        self._draw_cascadeur_controls(layout)
+        self._draw_blender_to_cascadeur(layout, addon_props)
+        self._draw_cascadeur_to_blender(layout, addon_props)
+
+    def _draw_verification(self, layout, cascadeur_handler):
+        errors = []
+
+        if not cascadeur_handler.is_csc_exe_path_valid:
+            errors.append("No valid Cascadeur exe path set in preferences!")
+
+        if not cascadeur_handler.is_csc_bridge_installed:
+            errors.append("Cascadeur side of the add-on is not installed!")
+
+        if not errors:
+            return True
+
+        col = layout.column()
+
+        for error in errors:
+            col.label(icon="ERROR", text=error)
+
+        col.separator()
+        col.operator(
+            "cbb.open_preferences",
+            text="Open Preferences",
+            icon="PREFERENCES",
+        )
+
+        return False
+
+    def _draw_cascadeur_controls(self, layout):
+        col = layout.column()
+
+        row = col.row()
         row.operator(
             "cbb.start_cascadeur",
             text="Start Cascadeur",
             icon="MESH_UVSPHERE",
         )
         row.scale_y = 1.5
+
         col.separator()
 
+    def _draw_operation_status(self, layout):
         if not addon_info.operation_completed:
+            col = layout.column()
             col.label(icon="LOCKED", text="Operation in progress!")
-            col.separator()
+            return True
+        else:
+            return False
 
-        # Blender to Cascadeur
+    def _draw_blender_to_cascadeur(self, layout, addon_props):
         box = layout.box()
         col = box.column()
+
         row = col.row()
         row.label(text="Blender > Cascadeur")
         row.scale_y = 1.2
+
         row = col.row()
         row.prop(addon_props, "cbb_import_methods")
         row.scale_y = 1.2
+
         row = col.row()
         row.operator(
-            "cbb.export_blender_fbx", text="Export To Cascadeur", icon="EXPORT"
+            "cbb.export_blender_fbx",
+            text="Export To Cascadeur",
+            icon="EXPORT",
         )
-        # Cascadeur to Blender
+
+    def _draw_cascadeur_to_blender(self, layout, addon_props):
         box = layout.box()
         col = box.column(align=True)
+
         row = col.row()
         row.label(text="Cascadeur > Blender")
         row.scale_y = 1.2
+
         row = col.row()
         row.prop(addon_props, "cbb_export_methods")
-        props = col.operator(
+
+        self._draw_import_actions(col)
+        self._draw_batch_import(col)
+
+    def _draw_import_actions(self, layout):
+        props = layout.operator(
             "cbb.import_cascadeur_action",
             text="Import Action",
             icon="ARMATURE_DATA",
         )
         props.batch_export = False
-        props = col.operator(
+
+        props = layout.operator(
             "cbb.import_cascadeur_fbx",
             text="Import Scene",
             icon="IMPORT",
         )
         props.batch_export = False
-        # Batch
-        col = box.column(align=True)
-        col.label(text="Batch Import")
-        props = col.operator(
+
+    def _draw_batch_import(self, layout):
+        layout.separator()
+        layout.label(text="Batch Import")
+
+        props = layout.operator(
             "cbb.import_cascadeur_action",
             text="Import All Actions",
             icon="CON_ARMATURE",
         )
         props.batch_export = True
-        props = col.operator(
+
+        props = layout.operator(
             "cbb.import_cascadeur_fbx",
             text="Import All Scenes",
             icon="DOCUMENTS",
